bee76c7a61e8b9763a6818c6770b54dbe26841fe
Muhammad15246 <faizybangash05@gmail.com>
2025-06-17 09:01:42 +0500

    changes-in-auth-service

--- src/App.ts
+++ src/App.ts
@@ -14,7 +14,13 @@ app.use(express.static('public')); // Serve static files from the 'public'
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }),
+);
 
 // Routes
 app.get('/', (req, res, next) => {
